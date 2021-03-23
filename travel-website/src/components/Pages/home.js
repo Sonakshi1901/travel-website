06fd0335db75fef418711c4ed0aa6591fbf52c07
Sonakshi1901 <sonakshi1901@gmail.com>
2021-03-24 00:30:42 +0400

    Pages

---
+++ travel-website/src/components/Pages/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../../App.css';
+import Main from '../Main_Section/main';
+import CardList from '../Cards/CardList';
+
+function Home() {
+  return(
+    <>
+     <Main />
+     <CardList />
+    </>
+  )
+}
+
+export default Home;
---
+++ travel-website/src/components/Pages/sign_up.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../App.css";
+
+function SignUp() {
+  return <h1 className="sign_up">SIGN UP</h1>
+}
+
+export default SignUp;
