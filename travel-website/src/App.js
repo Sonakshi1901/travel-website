b76a1060bdfda7eb86c35a295c03cfea424d33aa
Sonakshi1901 <sonakshi1901@gmail.com>
2021-03-24 00:31:27 +0400

    App

--- travel-website/src/App.js
+++ travel-website/src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import Navbar from "./components/Navbar/navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Pages/home";
+import Products from "./components/Pages/products";
+import Services from "./components/Pages/services";
+import SignUp from "./components/Pages/sign_up";
 
 function App() {
   return (
@@ -9,7 +13,10 @@ function App() {
      <Router>
       <Navbar/>
       <Switch>
-        <Route path="/" exact />
+        <Route path="/" exact component={Home} />
+        <Route path="/services" exact component={Services} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/sign-up" exact component={SignUp} />
       </Switch>
      </Router>
     </>
