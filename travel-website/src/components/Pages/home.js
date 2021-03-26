1af63fc48c5a03ca326bea97e5adfe40587a043a
Sonakshi1901 <sonakshi1901@gmail.com>
2021-03-26 23:50:26 +0400

    Update home.js

--- travel-website/src/components/Pages/home.js
+++ travel-website/src/components/Pages/home.js
@@ -2,12 +2,14 @@ import React from "react";
 import '../../App.css';
 import Main from '../Main_Section/main';
 import CardList from '../Cards/CardList';
+import Footer from '../Footer/footer';
 
 function Home() {
   return(
     <>
      <Main />
      <CardList />
+     <Footer />
     </>
   )
 }
